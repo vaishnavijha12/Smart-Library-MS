d04c5525e0f75d64f2b5d295be2e2dbab636fb9a
Pritam Kumar <pritamk6284987295@gmail.com>
2025-10-19 00:03:14 +0530

    fix

--- components/home/FooterSection.tsx
+++ components/home/FooterSection.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { BookOpen, Github, Linkedin, Twitter, Mail } from "lucide-react";
-import {Link} from 'next/link'
+import Link from 'next/link'
 export default function FooterSection() {
   const year = new Date().getFullYear();
 
